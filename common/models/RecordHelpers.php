6c396327e4fbf5703658df7cee3a3c6761ed7bb3
vlad ivanov <vlad ivanov>
2016-12-02 14:42:49 +0300

    Helpers

---
+++ common/models/RecordHelpers.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vova
+ * Date: 02.12.2016
+ * Time: 14:29
+ */
+
+namespace common\models;
+
+use yii;
+Class RecordHelpers
+{
+    /**
+     * @param $model_name
+     * @return bool
+     */
+    public static function userHas($model_name)
+    {
+        $connection = \Yii::$app->db;
+        $userid = Yii::$app->user->identity->id;
+        $sql = "SELECT id FROM $model_name WHERE user_id=:userid";
+        $command = $connection->createCommand($sql);
+        $command->bindValue(":userid", $userid);
+        $result = $command->queryOne();
+        return $result == null ? false : $result['id'];
+    }
+}
